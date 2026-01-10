c8d1605cccd8db87f09cbda877415e30bf1b2bf2
Yaseen Ayatullah Khan <24216496@siswa.um.edu.my>
2026-01-10 15:51:53 +0000

    Updated Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,67 @@
-//student class and methods
+public class Student {
+    private String studentID;
+    private String name;
+    private ArrayList<Course> enrolledCourses;
+    private int maxCredits;
+    private static int totalCreditsEnrolled;
+
+    //constructors
+    public Student() {}
+    public Student(String studentID, String name, int maxCredits) {
+         this.name = name;
+         this.studentID = studentID;
+         this.maxCredits = maxCredits;
+         enrolledCourses = new ArrayList<>();
+         totalCreditsEnrolled = 0;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getMaxCredits() {
+        return maxCredits;
+    }
+    public int getTotalCreditsEnrolled() {
+        return totalCreditsEnrolled;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setMaxCredits(int maxCredits) {
+        this.maxCredits = maxCredits;
+    }
+
+    //enroll in a course
+    public boolean enrollInCourse(Course course) {
+        if (totalCreditsEnrolled + course.getCredits() <= maxCredits && course.getCurrentEnrollment() < course.getMaxCapacity()) {
+            enrolledCourses.add(course);
+            totalCreditsEnrolled += course.getCredits();
+            course.incrementEnrollment();
+            return true;
+        }
+        return false;
+    }
+    public void dropCourse(Course course) {
+        if (enrolledCourses.remove(course) == true) {
+            totalCreditsEnrolled -= course.getCredits();
+            course.decrementEnrollment();
+        }
+    }
+    public void displayStudentInfo() {
+        System.out.println("Student ID: " + this.studentID);
+        System.out.println("Name: " + this.name);
+        System.out.println("Maximum Credits Allowed: " + this.maxCredits);
+        System.out.println("Total Credits Enrolled: " + this.totalCreditsEnrolled);
+        System.out.println("Enrolled Courses: ");
+        for (Course course : enrolledCourses) {
+            System.out.println("- " + course.getCourseCode() + ": " + course.getCourseName());
+        }
+    }
+}
